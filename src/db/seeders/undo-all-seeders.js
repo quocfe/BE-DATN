8d2c5d51eef8d2ee01814a4f4c02f765bb2fb128
kanisdev <dinhcan.cntt@gmail.com>
2024-04-07 09:40:33 +0700

    setup

---
+++ src/db/seeders/undo-all-seeders.js
@@ -0,0 +1,10 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+const { hashSync, genSaltSync } = require('bcryptjs')
+module.exports = {
+  async up(queryInterface, Sequelize) {},
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Roles', null, {})
+    await queryInterface.bulkDelete('Users', null, {})
+  }
+}
